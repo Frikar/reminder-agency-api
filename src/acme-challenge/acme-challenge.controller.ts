c2bbf3f1ce3631e47c017f15cdd85e61d0ba33f0
Frikar <ddglobatravel1@gmail.com>
2023-04-23 10:18:22 -0500

    serve static v3

--- src/acme-challenge/acme-challenge.controller.ts
+++ src/acme-challenge/acme-challenge.controller.ts
@@ -1,17 +1,28 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { join } from 'path';
-import { promises as fs } from 'fs';
+import { createReadStream } from 'fs';
 
 @Controller('.well-known/acme-challenge')
 export class AcmeChallengeController {
   @Get(':token')
-  async getChallenge(@Param('token') token: string, @Res() res: Response) {
+  @Get(':challenge')
+  async serveChallenge(
+    @Param('challenge') challenge: string,
+    @Res() res: Response,
+  ) {
+    const challengePath = join(
+      __dirname,
+      '..',
+      'public',
+      '.well-known',
+      'acme-challenge',
+      challenge,
+    );
+
     try {
-      const filePath = join('/var/www/html/.well-known/acme-challenge', token);
-      const fileContent = await fs.readFile(filePath, 'utf8');
-      res.send(fileContent);
-    } catch (err) {
+      createReadStream(challengePath).pipe(res);
+    } catch (error) {
       res.status(404).send('Not found');
     }
   }
